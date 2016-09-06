3fe9fcbc39849ac65b2bf771d93d916c59e2a879
Andrea Levy <alevy05@calpoly.edu>
2016-09-05 18:23:44 -0700

    Now supports multiple currencies and an new UI
    This new version is no longer based off of the Udemy Course

--- app/src/main/java/com/example/andrea/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/andrea/currencyconverter/MainActivity.java
@@ -1,28 +1,138 @@
 package com.example.andrea.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-    public void convertToPounds(View view)
-    {
-        EditText amountEntered = (EditText) findViewById(R.id.amount);
-        Double dollarAmount = Double.parseDouble(amountEntered.getText().toString());
-        Log.i("Dollar Amount", amountEntered.getText().toString());
-        Double poundAmount = dollarAmount * 0.75;
-        String pounds = poundAmount.toString();
-        Toast.makeText(getApplicationContext(), "£" + pounds, Toast.LENGTH_LONG).show();
-    }
+public class MainActivity extends AppCompatActivity
+{
+   public JSONObject rates;
+   public static ArrayList<CharSequence> countryCodes = new ArrayList<>(170);
+   public static Spinner spinner;
+   public static ArrayAdapter<CharSequence> adapter;
+   public static String code = "USD";
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      spinner = (Spinner) findViewById(R.id.spinner);
+      adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, countryCodes);
+      spinner.setAdapter(adapter);
+      DownloadTask downloadTask = new DownloadTask();
+      downloadTask.execute("http://apilayer.net/api/live?access_key=0167f2a30958c345d3e713c9ea44abe1&format=1");
+
+
+   }
+
+
+   public void viewConvertCurrency(View view)
+   {
+      try
+      {
+         EditText dollarAmount = (EditText) findViewById(R.id.dollarAmount);
+         Double dollars = Double.parseDouble(dollarAmount.getText().toString());
+         String toFrom = getString(R.string.usd)+ code;
+         Double multiplier = rates.getDouble(toFrom);
+         Double amount = Math.round(dollars * multiplier * 100) / 100.0;
+         TextView toCurrency = (TextView) findViewById(R.id.toCurrency);
+         toCurrency.setText(Double.toString(amount));
+      }
+      catch (JSONException e)
+      {
+         Log.e("Multiplier", "no double found");
+      }
+
+   }
+
+
+
+   public class DownloadTask extends AsyncTask<String, Void, String>
+   {
+      @Override
+      protected String doInBackground(String... urls) {
+         String result = "";
+         URL url;
+         HttpURLConnection urlConnection;
+         try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int data = reader.read();
+            while(data != -1)
+            {
+               char current = (char) data;
+               result += current;
+               data = reader.read();
+            }
+            return result;
+         }
+         catch (IOException e) {
+            e.printStackTrace();
+         }
+         return null;
+      }
+
+      @Override
+      protected void onPostExecute(String result) {
+         super.onPostExecute(result);
+         try {
+            JSONObject jsonObject = new JSONObject(result);
+            String quotes = jsonObject.getString("quotes");
+            rates = new JSONObject(quotes);
+            Iterator<String> stringIterator = rates.keys();
+            while (stringIterator.hasNext())
+            {
+               String toFrom = stringIterator.next();
+               String code = toFrom.substring(3);
+               countryCodes.add(code);
+            }
+
+            adapter.notifyDataSetChanged();
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+            {
+               @Override
+               public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+               {
+                  code = parent.getItemAtPosition(pos).toString();
+                  spinner.setSelection(pos);
+               }
+
+
+               @Override
+               public void onNothingSelected(AdapterView<?> parent)
+               {
+                  spinner.setSelection(0);
+                  code = parent.getItemAtPosition(0).toString();
+               }
+            });
+
+
+
+         } catch (JSONException e) {
+            e.printStackTrace();
+         }
+
+      }
+   }
 }
 
